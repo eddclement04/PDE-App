15c2f8d2a6df5974a246cb39dbaf8edd57efe3a5
eddclement04 <eddclement04@gmail.com>
2026-08-07 17:28:18 -0400

    Load quick add dropdown-only version

--- pde-job-details.js
+++ pde-job-details.js
@@ -123,7 +123,7 @@
     loadScriptOnce('pdeEditEnhanceScript','pde-edit-enhance.js?v=5');
     loadScriptOnce('pdeDropboxSyncScript','pde-dropbox-sync.js?v=4');
     loadScriptOnce('pdeHamburgerMenuScript','pde-hamburger-menu.js?v=2');
-    loadScriptOnce('pdeQuickAddScript','pde-quick-add.js?v=2');
+    loadScriptOnce('pdeQuickAddScript','pde-quick-add.js?v=3');
   });
   window.addEventListener('storage',renderJobCostDetails);
   document.addEventListener('submit',()=>setTimeout(renderJobCostDetails,500),true);
